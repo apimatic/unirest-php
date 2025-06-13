fb8ed12405f11145c3dd5d22050cceee1d386f9f
M.Hamza Shahzad <hamzashahzad664343@gmail.com>
2025-06-13 11:11:24 +0500

    fix(post-request): handle empty body

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -115,15 +115,12 @@ class HttpClient implements HttpClientInterface
         if ($request->getHttpMethod() !== RequestMethod::GET) {
             if ($request->getHttpMethod() === RequestMethod::POST) {
                 curl_setopt($handle, CURLOPT_POST, true);
+                curl_setopt($handle, CURLOPT_POSTFIELDS, is_null($body) ? [] : $body);
             } else {
                 if ($request->getHttpMethod() === RequestMethod::HEAD) {
                     curl_setopt($handle, CURLOPT_NOBODY, true);
                 }
                 curl_setopt($handle, CURLOPT_CUSTOMREQUEST, strtoupper($request->getHttpMethod()));
-            }
-
-            if (!is_null($body)) {
-                curl_setopt($handle, CURLOPT_POSTFIELDS, $body);
             }
         } elseif (is_array($body)) {
             if (strpos($queryUrl, '?') !== false) {
