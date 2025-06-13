70e0f4280e0e779918f39ce03bec7fe1417d5641
M.Hamza Shahzad <hamzashahzad664343@gmail.com>
2025-06-13 15:26:50 +0500

    fix(post-request): handle empty body Tested

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -107,11 +107,11 @@ class HttpClient implements HttpClientInterface
         }
         return $multipartParameters;
     }
-
     protected function setCurlOptions($handle, RequestInterface $request): void
     {
         $queryUrl = $request->getQueryUrl();
         $body = $this->getBody($request);
+
         if ($request->getHttpMethod() !== RequestMethod::GET) {
             if ($request->getHttpMethod() === RequestMethod::POST) {
                 curl_setopt($handle, CURLOPT_POST, true);
@@ -121,6 +121,10 @@ class HttpClient implements HttpClientInterface
                     curl_setopt($handle, CURLOPT_NOBODY, true);
                 }
                 curl_setopt($handle, CURLOPT_CUSTOMREQUEST, strtoupper($request->getHttpMethod()));
+
+                if (!is_null($body)) {
+                    curl_setopt($handle, CURLOPT_POSTFIELDS, $body);
+                }
             }
         } elseif (is_array($body)) {
             if (strpos($queryUrl, '?') !== false) {
@@ -139,10 +143,7 @@ class HttpClient implements HttpClientInterface
             CURLOPT_HTTPHEADER => $this->getFormattedHeaders($request),
             CURLOPT_HEADER => true,
             CURLOPT_SSL_VERIFYPEER => $this->config->shouldVerifyPeer(),
-            // CURLOPT_SSL_VERIFYHOST accepts only 0 (false) or 2 (true).
-            // Future versions of libcurl will treat values 1 and 2 as equals
             CURLOPT_SSL_VERIFYHOST => $this->config->shouldVerifyHost() === false ? 0 : 2,
-            // If an empty string, '', is set, a header containing all supported encoding types is sent
             CURLOPT_ENCODING => ''
         ];
 
@@ -163,20 +164,20 @@ class HttpClient implements HttpClientInterface
 
         if (!empty($this->config->getAuth()['user'])) {
             curl_setopt_array($handle, [
-                CURLOPT_HTTPAUTH    => $this->config->getAuth()['method'],
-                CURLOPT_USERPWD     => $this->config->getAuth()['user'] . ':' . $this->config->getAuth()['pass']
+                CURLOPT_HTTPAUTH => $this->config->getAuth()['method'],
+                CURLOPT_USERPWD => $this->config->getAuth()['user'] . ':' . $this->config->getAuth()['pass']
             ]);
         }
 
         $proxy = $this->config->getProxy();
         if (!empty($proxy['address'])) {
             curl_setopt_array($handle, [
-                CURLOPT_PROXYTYPE       => $proxy['type'],
-                CURLOPT_PROXY           => $proxy['address'],
-                CURLOPT_PROXYPORT       => $proxy['port'],
+                CURLOPT_PROXYTYPE => $proxy['type'],
+                CURLOPT_PROXY => $proxy['address'],
+                CURLOPT_PROXYPORT => $proxy['port'],
                 CURLOPT_HTTPPROXYTUNNEL => $proxy['tunnel'],
-                CURLOPT_PROXYAUTH       => $proxy['auth']['method'],
-                CURLOPT_PROXYUSERPWD    => $proxy['auth']['user'] . ':' . $proxy['auth']['pass']
+                CURLOPT_PROXYAUTH => $proxy['auth']['method'],
+                CURLOPT_PROXYUSERPWD => $proxy['auth']['user'] . ':' . $proxy['auth']['pass']
             ]);
         }
     }
